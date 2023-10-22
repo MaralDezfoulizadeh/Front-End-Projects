c7b72150f00200df85ea320bccc98478ad7c2d72
Maral <99055489+MaralDezfoulizadeh@users.noreply.github.com>
2023-10-22 16:16:41 +0330

    Delete public directory

--- public/JS files/darkmodeToggle.js
+++
@@ -1,6 +0,0 @@
-const content = document.getElementsByTagName("body")[0];
-const DarkMode = document.querySelector(".darkModeToggle");
-DarkMode.addEventListener("click", function () {
-  DarkMode.classList.toggle("active");
-  content.classList.toggle("night");
-});
